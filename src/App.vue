d0983daac51125d18c8c9f0b4f4862ff4b96d4a0
mhamad raad <hamaraad883@gmail.com>
2025-05-19 00:35:37 +0300

    add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,5 @@
-<script setup>
-import { ref, computed } from 'vue';
-
-import HelloWorld from './components/HelloWorld.vue';
-
-const count = ref(0);
-
-const increment = () => {
-  if (count.value < 10) count.value++;
-  else return;
-};
-
-const msg = computed(() => (count.value > 5 ? 'Passed' : 'Failed'));
-</script>
-
 <template>
-  <HelloWorld :msg="msg" :onClick="increment" :count="count" />
+  <h1>Vue 3 + Vite + Vue Router</h1>
+  <router-view />
 </template>
-
-<style scoped></style>
 
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <h2>About</h2>
+  <p>This is the about page.</p>
+</template>
+
+<style scoped></style>
